cd9387708242caca65834f69fc66c2d0387279e0
Luiz Júnior <34041344+LuizAPJunior@users.noreply.github.com>
2026-03-17 11:24:06 -0300

    feat(api): adicionando endpoint de cadastro de usuário(cliente)

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-const express = require('express');
+const express = require('express')
 const supabaseJS = require('@supabase/supabase-js')
+
+
 
 const app = express()
 app.use(express.json())
@@ -15,7 +17,32 @@ if (!supabaseUrl || !supabaseAnonKey) {
 const supabase = supabaseJS.createClient(supabaseUrl, supabaseAnonKey)
 
 app.get('/', async (req, res) => {
-  const { data, error } = await supabase.from('Itens').select('*')
+  const { data, error } = await supabase.from('itens').select('*')
+  if (error) return res.status(500).json({ error: error.message })
+  res.json(data)
+})
+
+
+app.post('/cadastro', async (req, res) => {
+
+  const { email, password, ...metadata } = req.body;
+  const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: metadata,       
+      }
+    })
+  if (error) return res.status(500).json({ error: error.message })
+  res.json(data)    
+})
+
+
+//testando rota para verificar se a RLS está funcionando
+app.delete('/itens/:id', async (req, res) => {
+  const itemId  = req.params.id 
+  console.log(itemId)
+  const {data, error} = await supabase.from('itens').delete().eq('id', itemId)
   if (error) return res.status(500).json({ error: error.message })
   res.json(data)
 })
