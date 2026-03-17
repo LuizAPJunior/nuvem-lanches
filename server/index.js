f99812ad9d5baca7881a02cee96dfade07fdac32
Luiz Júnior <34041344+LuizAPJunior@users.noreply.github.com>
2026-03-17 17:21:54 -0300

    feat(api) adicionar endpoint de autenticação do usuário

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const supabaseJS = require('@supabase/supabase-js')
-
+const jwt = require('jsonwebtoken');
 
 
 const app = express()
@@ -24,7 +24,6 @@ app.get('/', async (req, res) => {
 
 
 app.post('/cadastro', async (req, res) => {
-
   const { email, password, ...metadata } = req.body;
   const { data, error } = await supabase.auth.signUp({
       email,
@@ -36,6 +35,88 @@ app.post('/cadastro', async (req, res) => {
   if (error) return res.status(500).json({ error: error.message })
   res.json(data)    
 })
+
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
+
+  if (error){ 
+    if(error.code === "email_not_confirmed"){
+      await supabase.auth.resend({
+        email: email,
+        type: 'signup',
+      })
+      console.log(error);
+      console.log(error.message);
+      
+      return res.status(401).json({ error: 'email não confirmado, um novo email de confirmação está sendo enviado para o seu email.'}) 
+    }
+    return res.status(401).json({ error: error.message }) 
+  }
+  
+  // The session object contains the access token (JWT) and refresh token
+  const { session } = data;
+
+
+  res.json({ 
+    message: 'Logado!', 
+    access_token: session.access_token,
+    refresh_token: session.refresh_token,
+    user: session.user 
+  })
+
+})
+
+
+
+const authenticateUser = async (req, res, next) => {
+  const authHeader = req.headers.authorization
+ 
+  
+  if (!authHeader) {
+    return res.status(401).json({ error: 'token não fornecido' })
+  }
+
+  const token = authHeader.split(' ')[1]; // "Bearer <token>"
+  console.log(token)
+  const decoded = jwt.decode(token) 
+  console.log(decoded)
+  
+ 
+  const { data: { user }, error } = await supabase.auth.getUser(token)
+  console.log(user)
+  
+  if (error) {
+    console.log(error)
+    
+    return res.status(401).json({ error: 'token inválido' })
+  }
+  req.user = user
+  next()
+
+}
+
+
+app.post('/logout', async (req, res) => {
+  const token = req.headers.authorization?.split(' ')[1] 
+  if (!token) return res.sendStatus(204) 
+
+  const { error } = await supabase.auth.admin.signOut(token)
+
+  if (error) {
+    console.log(error)
+    if(error.code === 'bad_jwt') return res.status(403).json({ error: 'token inválido' })
+
+    return res.status(401).json({ error: 'token não fornecido' })
+  }
+
+  res.json({ message: 'Deslogado!' })
+})
+
 
 
 //testando rota para verificar se a RLS está funcionando
