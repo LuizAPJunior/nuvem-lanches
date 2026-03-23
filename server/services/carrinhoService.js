cd1557a70289f4337453c6d6600e22caa9b874f5
Luiz Júnior <34041344+LuizAPJunior@users.noreply.github.com>
2026-03-23 15:05:03 -0300

    refactor(carrinho): removendo código desnecessário no carrinho

--- server/services/carrinhoService.js
+++ server/services/carrinhoService.js
@@ -88,34 +88,3 @@ exports.updateQuantity = async (supabase, {userId, carrinhoId, action}) => {
 
   return quantityUpdated;   
 }
-
-
-
-//carrinhoId é o id  da "linha" do carrinho, não é id do item
-exports.subtractQuantity = async (supabase, {userId, carrinhoId}) => {
-
-  //selecionar a quantidade do item   
-  const { data, error } = await supabase
-  .from('carrinho_itens')
-  .select('quantidade')
-  .eq('id', carrinhoId)
-  .eq('perfil_id', userId)
-  .select();
-
-  if (error) throw error;
-  if (!data || data.length === 0) throw { status: 404, message: 'Nenhum item foi encontrado no carrinho.' };
-   
-  const quantidadeAtualizada = data[0].quantidade - 1;  
-
-  //atualizar o valor do item
-  const { data: quantityUpdated, error: quantityError } = await supabase
-  .from('carrinho_itens') 
-  .update({quantidade: quantidadeAtualizada})
-  .eq('id', carrinhoId)
-  .eq('perfil_id', userId)
-  .select();
- 
-  if (quantityError) throw quantityError;
-
-  return quantityUpdated;   
-}
